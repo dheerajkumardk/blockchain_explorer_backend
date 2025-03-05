0fb9a5422de8f2d3daef0fdfa0297c1b19d68819
Dheeraj <dyaduwanshi0@gmail.com>
2025-03-05 21:26:15 +0530

    feat: add routes

---
+++ routes/route.go
@@ -0,0 +1,52 @@
+package routes
+
+import (
+	"github.com/dheerajkumardk/blockchain_explorer_backend/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+// lists all routes provided by the explorer
+func SetupRoutes(app *fiber.App) {
+	// Get all blocks - limit, paginated
+	app.Get("/blocks", handlers.GetAllBlocks)
+
+	// Retrieve info abt a block
+	app.Get("/blocks/:blockNumber", handlers.GetBlockByBlockNumber)
+
+	// Get all txnx by blockNumber
+	app.Get("blocks/:block/transactions", handlers.GetBlockTransactions)
+
+	// Get all txns -> paginated list [page, limit, sort by timestamp]
+	app.Get("/transactions", handlers.GetAllTransactions)
+
+	// Retrieve txn info by hash
+	app.Get("/transactions/:txHash", handlers.GetTransactionInfo)
+
+	// Get all txns for an address
+	app.Get("/accounts/:address/transactions", handlers.AccountTransactions)
+
+	// Get all withdrawals
+	app.Get("/withdrawals", handlers.GetAllWithdrawals)
+
+	// Retrieve withdrawal by index
+	app.Get("/withdrawals/:index", handlers.GetWithdrawalInfo)
+
+	// Get info abt an account
+	app.Get("/accounts/:address", handlers.GetAccountInfo)
+
+	// Get balance of an account
+	app.Get("/accounts/:address/balance", handlers.GetAccountBalance)
+
+	// Search End-points
+	// Search for blocks (blockNumber), transactions(txnHash), or accounts(Address)
+	app.Get("/search")
+
+	// Post a new block
+	// app.Post("/blocks")
+
+	// Post a new transaction
+	// app.Post("/transactions")
+
+	// Post a new withdrawl
+	// app.Post("/withdrawals")
+}
