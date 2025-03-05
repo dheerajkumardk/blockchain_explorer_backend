83b44d94ca456d2f4e9a9841eec36a79ef60ffb3
Dheeraj <dyaduwanshi0@gmail.com>
2025-03-05 21:25:02 +0530

    initialise db

---
+++ database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// database object
+var BlockDB *gorm.DB
+
+func InitDatabase() {
+	// 1. We need GORM and db drivers
+	// 2. Connect to the db using Gorm's Open Method
+	var err error
+	BlockDB, err := gorm.Open(sqlite.Open("blockchain.db"))
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+	fmt.Println("Database connected!")
+	// 3. Define models/structs for db schemas
+	// 4. AutoMigrate - Create/update dB schemas based on our models
+	err = BlockDB.AutoMigrate(&Block{}, &Transaction{}, &Withdrawal{}, &Account{})
+	if err != nil {
+		panic("failed to migrate")
+	}
+	fmt.Println("Database Migrated.")
+}
