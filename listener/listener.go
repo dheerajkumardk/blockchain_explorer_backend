f07bc03f081deb151ce2d251d592ae7c8990f9c1
Dheeraj <dyaduwanshi0@gmail.com>
2025-03-09 12:42:23 +0530

    handle error for db write ops

--- listener/listener.go
+++ listener/listener.go
@@ -2,7 +2,7 @@ package listener
 
 import (
 	"context"
-	"fmt"
+	_ "fmt"
 	"log"
 	"os"
 
@@ -44,91 +44,106 @@ func SubscribeBlocks() {
 
 			// create block to insert into db
 			newBlock := database.Block{
-				BlockNumber: block.Number().Uint64(),
-				Timestamp: header.Time,
-				FeeRecipient: header.Coinbase.String(),
-				BlockReward: "",
+				BlockNumber:     block.Number().Uint64(),
+				Timestamp:       header.Time,
+				FeeRecipient:    header.Coinbase.String(),
+				BlockReward:     "",
 				TotalDifficulty: header.Difficulty.Uint64(),
-				Size: block.Size(),
-				GasUsed: header.GasUsed,
-				GasLimit: header.GasLimit,
-				BaseFeePerGas: header.BaseFee.String(),
-				BurntFees: "",
-				Hash: block.Hash().String(),
-				ParentHash: header.ParentHash.Hex(),
-				StateRoot: header.Root.Hex(),
+				Size:            block.Size(),
+				GasUsed:         header.GasUsed,
+				GasLimit:        header.GasLimit,
+				BaseFeePerGas:   header.BaseFee.String(),
+				BurntFees:       "",
+				Hash:            block.Hash().String(),
+				ParentHash:      header.ParentHash.Hex(),
+				StateRoot:       header.Root.Hex(),
 				WithdrawalsRoot: header.WithdrawalsHash.String(),
-				Nonce: header.Nonce.Uint64(),
+				Nonce:           header.Nonce.Uint64(),
+			}
+
+			db := database.BlockDB
+			if db == nil {
+				log.Print("DB is nil")
+			} else {
+				log.Print("All OK!")
 			}
 			// insert block into db
-			db := database.BlockDB
-			db.Create(&newBlock)
+			result := db.Create(&newBlock)
+			if result.Error != nil {
+				log.Printf("Failed to insert block %d %v", block.Number(), result.Error)
+			} else {
+				log.Printf("Stored block %d\n", block.Number())
+			}
 
 			// console log the data
-			fmt.Println("block Number: ", block.Number().Int64())
-			fmt.Println("BlockHash: ", block.Hash().String())
-			fmt.Println("Timestamp: ", header.Time)
-			fmt.Println("Proposed On: ")
-			fmt.Println("Transactions: ", block.Transactions())
-			fmt.Println("Withdrawals: ", block.Withdrawals())
-			fmt.Println("Fee Recipient", header.Coinbase)
-			fmt.Println("Block Reward")
-			fmt.Println("Total Difficulty", header.Difficulty)
-			fmt.Println("Size", block.Size())
-			fmt.Println("GasUsed", header.GasUsed)
-			fmt.Println("Gas Limit", header.GasLimit)
-			fmt.Println("BaseFeePerGas", header.BaseFee)
-			fmt.Println("BurntFees", uint64(header.BaseFee.Int64()) + header.GasUsed)
-			fmt.Println("Extra Data", header.Extra)
-			fmt.Println("Hash", block.Hash().String())
-			fmt.Println("Parent Hash", header.ParentHash)
-			fmt.Println("StateRoot", header.Root)
-			fmt.Println("WithdrawalsRoot", header.WithdrawalsHash)
-			fmt.Println("Nonce", header.Nonce.Uint64())
-
+			// fmt.Println("block Number: ", block.Number().Int64())
+			// fmt.Println("BlockHash: ", block.Hash().String())
+			// fmt.Println("Timestamp: ", header.Time)
+			// fmt.Println("Proposed On: ")
+			// fmt.Println("Transactions: ", block.Transactions())
+			// fmt.Println("Withdrawals: ", block.Withdrawals())
+			// fmt.Println("Fee Recipient", header.Coinbase)
+			// fmt.Println("Block Reward")
+			// fmt.Println("Total Difficulty", header.Difficulty)
+			// fmt.Println("Size", block.Size())
+			// fmt.Println("GasUsed", header.GasUsed)
+			// fmt.Println("Gas Limit", header.GasLimit)
+			// fmt.Println("BaseFeePerGas", header.BaseFee)
+			// fmt.Println("BurntFees", uint64(header.BaseFee.Int64())+header.GasUsed)
+			// fmt.Println("Extra Data", header.Extra)
+			// fmt.Println("Hash", block.Hash().String())
+			// fmt.Println("Parent Hash", header.ParentHash)
+			// fmt.Println("StateRoot", header.Root)
+			// fmt.Println("WithdrawalsRoot", header.WithdrawalsHash)
+			// fmt.Println("Nonce", header.Nonce.Uint64())
 
 			// transactions
-			for index, tx := range block.Transactions() {
+			for _, tx := range block.Transactions() {
 				// create txn obj to insert into db
 				newTxn := database.Transaction{
-					TxHash: tx.Hash().String(),
+					TxHash:      tx.Hash().String(),
 					BlockNumber: block.Number().Uint64(),
-					From: "",
-					To: tx.To().String(),
-					Value: tx.Value().String(),
-					TxnFees: "",
-					Timestamp: uint64(tx.Time().Unix()),
-
+					From:        "",
+					To:          tx.To().String(),
+					Value:       tx.Value().String(),
+					TxnFees:     "",
+					Timestamp:   uint64(tx.Time().Unix()),
 				}
 				// insert txn into db
-				db.Create(&newTxn)
+				result = db.Create(&newTxn)
+				if result.Error != nil {
+					log.Printf("Failed to store txn %s", tx.Hash().String())
+				}
 
-				fmt.Println("txn: ", index)
-				fmt.Println("hash: ", tx.Hash().String())
-				fmt.Println("time: ", tx.Time())
-				fmt.Println("value ", tx.Value().String())
-				fmt.Println("gas ", tx.Gas())
-				fmt.Println("gas price ", tx.GasPrice().Uint64())
-				fmt.Println("nonce ", tx.Nonce())
-				fmt.Println("Data ", tx.Data())
-				fmt.Println("to", tx.To())
+				// fmt.Println("txn: ", index)
+				// fmt.Println("hash: ", tx.Hash().String())
+				// fmt.Println("time: ", tx.Time())
+				// fmt.Println("value ", tx.Value().String())
+				// fmt.Println("gas ", tx.Gas())
+				// fmt.Println("gas price ", tx.GasPrice().Uint64())
+				// fmt.Println("nonce ", tx.Nonce())
+				// fmt.Println("Data ", tx.Data())
+				// fmt.Println("to", tx.To())
 
-				fmt.Println()
+				// fmt.Println()
 			}
 
 			// withdrawals
 			for _, withdrawl := range block.Withdrawals() {
 				// create withdraw obj to insert into db
 				newWithdrawl := database.Withdrawal{
-					Index: withdrawl.Index,
-					BlockNumber: block.Number().Uint64(),
+					Index:          withdrawl.Index,
+					BlockNumber:    block.Number().Uint64(),
 					ValidatorIndex: withdrawl.Validator,
-					Recipient: withdrawl.Address.String(),
-					Amount: withdrawl.Amount,
+					Recipient:      withdrawl.Address.String(),
+					Amount:         withdrawl.Amount,
 				}
 
 				// insert withdrawal into db
-				db.Create(&newWithdrawl)
+				result = db.Create(&newWithdrawl)
+				if result.Error != nil {
+					log.Printf("Failed to store withdrawal %d", withdrawl.Index)
+				}
 			}
 
 		}
